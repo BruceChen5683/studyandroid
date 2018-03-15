0672c25d71a72d2da79db2fdfaa9cbda99d34302
chen.jianliang <chen.jianliang@whaley.cn>
2018-03-15 15:29:11 +0800

    constraintLayout

--- app/src/main/java/www/battlecall/tk/basedemo/ConstraintLayoutActivity.java
+++ app/src/main/java/www/battlecall/tk/basedemo/ConstraintLayoutActivity.java
@@ -3,11 +3,11 @@ package www.battlecall.tk.basedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ConstraintLayoutActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.guidline);
+		setContentView(R.layout.constraint_layout_guidline);
 	}
 }
