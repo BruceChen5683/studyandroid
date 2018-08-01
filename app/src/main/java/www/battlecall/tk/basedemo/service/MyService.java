09cc7223b75a05d2869f15c678a8351044862295
chen.jianliang <chen.jianliang@whaley.cn>
2018-08-01 19:58:34 +0800

    onstartCommand  Mode

--- app/src/main/java/www/battlecall/tk/basedemo/service/MyService.java
+++ app/src/main/java/www/battlecall/tk/basedemo/service/MyService.java
@@ -1,12 +1,25 @@
 package www.battlecall.tk.basedemo.service;
 
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.util.Log;
 
 public class MyService extends Service {
+
+	private final int DELAY = 2000;
+
+	private Handler mHandler = new Handler();
+	private Runnable mTask =  new Runnable(){
+		@Override
+		public void run() {
+			Log.d("cjl", "MyService ---------run:      "+DELAY/1000+"  s after");
+			Integer.parseInt("ok");
+		}
+	};
 	public MyService() {
 	}
 
@@ -16,16 +29,60 @@ public class MyService extends Service {
 		throw new UnsupportedOperationException("Not yet implemented");
 	}
 
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		Log.d("cjl", "MyService ---------onCreate:      ");
+		mHandler.postDelayed(mTask,DELAY);
 	}
+
+
+//	START_STICKY
+// 08-01 11:52:57.425 28062-28062/www.battlecall.tk.basedemo D/cjl: MyService ---------onCreate:
+//			08-01 11:52:57.425 28062-28062/www.battlecall.tk.basedemo D/cjl: MyService ---------onStartCommand:      startId  1
+//			08-01 11:52:57.425 28062-28062/www.battlecall.tk.basedemo D/cjl: MyService ---------onStartCommand:      intent Intent { cmp=www.battlecall.tk.basedemo/.service.MyService }
+//08-01 11:52:59.428 28062-28062/www.battlecall.tk.basedemo D/cjl: MyService ---------run:      2  s after
+	//Crash
+//08-01 11:53:20.187 30014-30014/? D/cjl: MyService ---------onCreate:
+//			08-01 11:53:20.187 30014-30014/? D/cjl: MyService ---------onStartCommand:      startId  2
+//			08-01 11:53:20.187 30014-30014/? D/cjl: MyService ---------onStartCommand:      intent null
+//			08-01 11:53:22.189 30014-30014/www.battlecall.tk.basedemo D/cjl: MyService ---------run:      2  s after
+
+//	START_STICKY_COMPATIBILITY
+//	08-01 11:54:52.756 30294-30294/www.battlecall.tk.basedemo D/cjl: MyService ---------onCreate:
+//			08-01 11:54:52.756 30294-30294/www.battlecall.tk.basedemo D/cjl: MyService ---------onStartCommand:      startId  1
+//			08-01 11:54:52.756 30294-30294/www.battlecall.tk.basedemo D/cjl: MyService ---------onStartCommand:      intent Intent { cmp=www.battlecall.tk.basedemo/.service.MyService }
+//08-01 11:54:54.759 30294-30294/www.battlecall.tk.basedemo D/cjl: MyService ---------run:      2  s after
+	//Crash
+//08-01 11:54:57.184 30559-30559/www.battlecall.tk.basedemo D/cjl: MyService ---------onCreate:
+//			08-01 11:54:59.188 30559-30559/www.battlecall.tk.basedemo D/cjl: MyService ---------run:      2  s after
+
+//	START_NOT_STICKY
+//	08-01 11:55:59.239 30770-30770/www.battlecall.tk.basedemo D/cjl: MyService ---------onCreate:
+//			08-01 11:55:59.239 30770-30770/www.battlecall.tk.basedemo D/cjl: MyService ---------onStartCommand:      startId  1
+//			08-01 11:55:59.239 30770-30770/www.battlecall.tk.basedemo D/cjl: MyService ---------onStartCommand:      intent Intent { cmp=www.battlecall.tk.basedemo/.service.MyService }
+//08-01 11:56:01.242 30770-30770/www.battlecall.tk.basedemo D/cjl: MyService ---------run:      2  s after
+	//Crash
+	//未重新创建
+
+//	START_REDELIVER_INTENT
+//	08-01 11:57:29.400 319-319/www.battlecall.tk.basedemo D/cjl: MyService ---------onCreate:
+//			08-01 11:57:29.401 319-319/www.battlecall.tk.basedemo D/cjl: MyService ---------onStartCommand:      startId  1
+//			08-01 11:57:29.401 319-319/www.battlecall.tk.basedemo D/cjl: MyService ---------onStartCommand:      intent Intent { cmp=www.battlecall.tk.basedemo/.service.MyService }
+//08-01 11:57:31.403 319-319/www.battlecall.tk.basedemo D/cjl: MyService ---------run:      2  s after
+	//Crash   startid  & intent
+//08-01 11:57:33.838 738-738/? D/cjl: MyService ---------onCreate:
+//			08-01 11:57:33.838 738-738/? D/cjl: MyService ---------onStartCommand:      startId  1
+//			08-01 11:57:33.839 738-738/? D/cjl: MyService ---------onStartCommand:      intent Intent { cmp=www.battlecall.tk.basedemo/.service.MyService }
+//08-01 11:57:35.839 738-738/www.battlecall.tk.basedemo D/cjl: MyService ---------run:      2  s after
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.d("cjl", "MyService ---------onStartCommand:      ");
-		return super.onStartCommand(intent, flags, startId);
+		Log.d("cjl", "MyService ---------onStartCommand:      startId  "+ startId);
+		Log.d("cjl", "MyService ---------onStartCommand:      intent "+intent);
+		return START_STICKY;
+//		return super.onStartCommand(intent, flags, startId);
 	}
 
 	@Override
