e7c6d4b222c5011d78416c28eb691b5eea246edb
chen.jianliang <chen.jianliang@whaley.cn>
2018-08-07 09:54:20 +0800

    js call android

--- app/src/main/java/www/battlecall/tk/basedemo/webview/WebViewActivity.java
+++ app/src/main/java/www/battlecall/tk/basedemo/webview/WebViewActivity.java
@@ -2,6 +2,7 @@ package www.battlecall.tk.basedemo.webview;
 
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,9 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Set;
 
 import www.battlecall.tk.basedemo.R;
 
@@ -43,19 +47,33 @@ public class WebViewActivity extends AppCompatActivity {
 
 //		mWebView.loadUrl("http://www.baidu.com/");
 //		mWebView.loadUrl("file:///android_asset/js.html");
-		mWebView.addJavascriptInterface(new AndroidtoJS(),"test");
+//		mWebView.addJavascriptInterface(new AndroidtoJS(),"test");
 		mWebView.loadUrl("file:///android_asset/callAndroidJs.html");
 ////		//设置不用系统浏览器打开,直接显示在当前Webview
-//		mWebView.setWebViewClient(new WebViewClient(){
-//			@Override
-//			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//				Log.d("cjl", "WebViewActivity ---------shouldOverrideUrlLoading:      "+url);
+		mWebView.setWebViewClient(new WebViewClient(){
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				Log.d("cjl", "WebViewActivity ---------shouldOverrideUrlLoading:      "+url);
 //				view.loadUrl(url);
 ////				view.loadUrl("http://www.hao123.com");
-//				return true;
-//			}
-//
-//		});
+				if (url.equals("js://webview?arg1=111&arg2=222")){
+					Uri uri = Uri.parse(url);
+					// 如果url的协议 = 预先约定的 js 协议
+					// 就解析往下解析参数
+					if (uri.getScheme().equals("js")){
+						if(uri.getAuthority().equals("webview")){
+							Log.d("cjl", "WebViewActivity ---------shouldOverrideUrlLoading:      js调用android方法2");
+							// 可以在协议上带有参数并传递到Android上
+							HashMap<String, String> params = new HashMap<>();
+							Set<String> collection = uri.getQueryParameterNames();
+						}
+						return true;
+					}
+				}
+				return super.shouldOverrideUrlLoading(view,url);
+			}
+
+		});
 
 		findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -82,42 +100,42 @@ public class WebViewActivity extends AppCompatActivity {
 
 
 
-		mWebView.setWebChromeClient(new WebChromeClient(){
-			@Override
-			public void onReceivedTitle(WebView view, String title) {
-				Log.d("cjl", "WebViewActivity ---------onReceivedTitle:      ");
-				mtitle.setText(title);
-			}
-
-			@Override
-			public void onProgressChanged(WebView view, int newProgress) {
-				if (newProgress < 100) {
-					String progress = newProgress + "%";
-					loading.setText(progress);
-				} else if (newProgress == 100) {
-					String progress = newProgress + "%";
-					loading.setText(progress);
-				}
-			}
-
-			@Override
-			public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-				Log.d("cjl", "WebViewActivity ---------onJsAlert:      "+message);
-				AlertDialog.Builder b = new AlertDialog.Builder(WebViewActivity.this);
-				b.setTitle("Alert");
-				b.setMessage(message);
-				b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						result.confirm();
-					}
-				});
-				b.setCancelable(false);
-				b.create().show();
-				return true;
-			}
-
-		});
+//		mWebView.setWebChromeClient(new WebChromeClient(){
+//			@Override
+//			public void onReceivedTitle(WebView view, String title) {
+//				Log.d("cjl", "WebViewActivity ---------onReceivedTitle:      ");
+//				mtitle.setText(title);
+//			}
+//
+//			@Override
+//			public void onProgressChanged(WebView view, int newProgress) {
+//				if (newProgress < 100) {
+//					String progress = newProgress + "%";
+//					loading.setText(progress);
+//				} else if (newProgress == 100) {
+//					String progress = newProgress + "%";
+//					loading.setText(progress);
+//				}
+//			}
+//
+//			@Override
+//			public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+//				Log.d("cjl", "WebViewActivity ---------onJsAlert:      "+message);
+//				AlertDialog.Builder b = new AlertDialog.Builder(WebViewActivity.this);
+//				b.setTitle("Alert");
+//				b.setMessage(message);
+//				b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+//					@Override
+//					public void onClick(DialogInterface dialog, int which) {
+//						result.confirm();
+//					}
+//				});
+//				b.setCancelable(false);
+//				b.create().show();
+//				return true;
+//			}
+//
+//		});
 
 
 //		mWebView.setWebViewClient(new WebViewClient(){
@@ -160,3 +178,7 @@ public class WebViewActivity extends AppCompatActivity {
 		super.onDestroy();
 	}
 }
+//TODO https://blog.csdn.net/carson_ho/article/details/64904691
+//TODO  方式3：通过 WebChromeClient 的onJsAlert()、onJsConfirm()、onJsPrompt（）方法回调拦截JS对话框alert()、confirm()、prompt（） 消息
+
+//TODO https://www.jianshu.com/p/3a345d27cd42  漏洞测试
