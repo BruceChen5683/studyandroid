23974068b6d1cb4ac84b0e97bc7abdcf65f4d8e5
chen.jianliang <chen.jianliang@whaley.cn>
2018-08-07 16:00:14 +0800

    js CallAndroid by webchromeclient

--- app/src/main/java/www/battlecall/tk/basedemo/webview/WebViewActivity.java
+++ app/src/main/java/www/battlecall/tk/basedemo/webview/WebViewActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JsPromptResult;
 import android.webkit.JsResult;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -48,31 +49,53 @@ public class WebViewActivity extends AppCompatActivity {
 //		mWebView.loadUrl("http://www.baidu.com/");
 //		mWebView.loadUrl("file:///android_asset/js.html");
 //		mWebView.addJavascriptInterface(new AndroidtoJS(),"test");
-		mWebView.loadUrl("file:///android_asset/callAndroidJs.html");
+//		mWebView.loadUrl("file:///android_asset/callAndroidJs.html");
 ////		//设置不用系统浏览器打开,直接显示在当前Webview
-		mWebView.setWebViewClient(new WebViewClient(){
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				Log.d("cjl", "WebViewActivity ---------shouldOverrideUrlLoading:      "+url);
-//				view.loadUrl(url);
-////				view.loadUrl("http://www.hao123.com");
-				if (url.equals("js://webview?arg1=111&arg2=222")){
-					Uri uri = Uri.parse(url);
-					// 如果url的协议 = 预先约定的 js 协议
-					// 就解析往下解析参数
-					if (uri.getScheme().equals("js")){
-						if(uri.getAuthority().equals("webview")){
-							Log.d("cjl", "WebViewActivity ---------shouldOverrideUrlLoading:      js调用android方法2");
-							// 可以在协议上带有参数并传递到Android上
-							HashMap<String, String> params = new HashMap<>();
-							Set<String> collection = uri.getQueryParameterNames();
-						}
-						return true;
-					}
-				}
-				return super.shouldOverrideUrlLoading(view,url);
-			}
+//		mWebView.setWebViewClient(new WebViewClient(){
+//			@Override
+//			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//				Log.d("cjl", "WebViewActivity ---------shouldOverrideUrlLoading:      "+url);
+////				view.loadUrl(url);
+//////				view.loadUrl("http://www.hao123.com");
+//				if (url.equals("js://webview?arg1=111&arg2=222")){
+//					Uri uri = Uri.parse(url);
+//					// 如果url的协议 = 预先约定的 js 协议
+//					// 就解析往下解析参数
+//					if (uri.getScheme().equals("js")){
+//						if(uri.getAuthority().equals("webview")){
+//							Log.d("cjl", "WebViewActivity ---------shouldOverrideUrlLoading:      js调用android方法2");
+//							// 可以在协议上带有参数并传递到Android上
+//							HashMap<String, String> params = new HashMap<>();
+//							Set<String> collection = uri.getQueryParameterNames();
+//						}
+//						return true;
+//					}
+//				}
+//				return super.shouldOverrideUrlLoading(view,url);
+//			}
+//
+//		});
 
+		mWebView.loadUrl("file:///android_asset/callAndroidJs3.html");
+		mWebView.setWebChromeClient(new WebChromeClient(){
+			@Override
+			public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {//or onJsAlert onJsConfirm
+				Log.d("cjl", "WebViewActivity ---------onJsPrompt:      url "+url);
+				Log.d("cjl", "WebViewActivity ---------onJsPrompt:      message"+message);
+				Uri uri = Uri.parse(message);
+
+				Log.d("cjl", "WebViewActivity ---------onJsPrompt:      "+uri.getScheme());
+				Log.d("cjl", "WebViewActivity ---------onJsPrompt:      "+uri.getAuthority());
+				if(uri.getScheme().equals("js")){
+					if(uri.getAuthority().equals("test3")){
+						Log.d("cjl", "WebViewActivity ---------onJsPrompt:      調用Android");
+
+						result.confirm("js調用了android的方法成功");
+					}
+					return true;
+				}
+				return super.onJsPrompt(view, url, message, defaultValue, result);
+			}
 		});
 
 		findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
@@ -178,7 +201,7 @@ public class WebViewActivity extends AppCompatActivity {
 		super.onDestroy();
 	}
 }
-//TODO https://blog.csdn.net/carson_ho/article/details/64904691
-//TODO  方式3：通过 WebChromeClient 的onJsAlert()、onJsConfirm()、onJsPrompt（）方法回调拦截JS对话框alert()、confirm()、prompt（） 消息
 
 //TODO https://www.jianshu.com/p/3a345d27cd42  漏洞测试
+
+//https://blog.csdn.net/carson_ho/article/details/64904691
