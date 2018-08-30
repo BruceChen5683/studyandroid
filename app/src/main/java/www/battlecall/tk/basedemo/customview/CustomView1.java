54ba2ddc7365454eb065a78e64537e93f97ffd4c
chen.jianliang <chen.jianliang@whaley.cn>
2018-08-30 16:16:10 +0800

    custom view

--- app/src/main/java/www/battlecall/tk/basedemo/customview/CustomView1.java
+++ app/src/main/java/www/battlecall/tk/basedemo/customview/CustomView1.java
@@ -2,13 +2,16 @@ package www.battlecall.tk.basedemo.customview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * Created by BattleCall on 2018/3/27.
@@ -23,19 +26,29 @@ public class CustomView1 extends ViewGroup{
 
 	public CustomView1(Context context) {
 		super(context);
+		setWillNotDraw(false);
 	}
 
 	public CustomView1(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		setWillNotDraw(false);
 	}
 
 	public CustomView1(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		setWillNotDraw(false);
 	}
 
 	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 	public CustomView1(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
 		super(context, attrs, defStyleAttr, defStyleRes);
+		setWillNotDraw(false);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		Log.d("cjl", "CustomView1 ---------onMeasure:      ");
 	}
 
 	@Override
@@ -52,5 +65,13 @@ public class CustomView1 extends ViewGroup{
 			int mBottom = mTop + height;
 			child.layout(mLeft,mTop,mRight,mBottom);
 		}
+		Log.d("cjl", "CustomView1 ---------onLayout:      ");
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		canvas.drawColor(Color.RED);
+		Log.d("cjl", "CustomView1 ---------onDraw:      ");
 	}
 }
